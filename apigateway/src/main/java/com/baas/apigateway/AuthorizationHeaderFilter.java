4eafb7b6e0397bd1ccde9f8ac1a3810f264b2fe6
rajendramani <rajendramani.r@gmail.com>
2022-01-20 18:30:48 +0530

    Add Authentication Filter

---
+++ apigateway/src/main/java/com/baas/apigateway/AuthorizationHeaderFilter.java
@@ -0,0 +1,19 @@
+package com.baas.apigateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+	public static class Config {
+
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
